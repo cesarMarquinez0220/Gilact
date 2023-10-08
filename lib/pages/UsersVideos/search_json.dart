2ac2bed3da75fc7bbb2e3a040314cbc672b5dc1b
Lexx_dg <48695091+jmg16@users.noreply.github.com>
2023-10-08 08:22:28 -0500

    Creación de pantalla de videos de usuarios
    - Se crea pantalla de videos para los usuarios.
    
    - se cambia nombre de la carpeta que contiene las miniaturas de los videos como mini_videos
    
    - se agregan nuevas imagenes para la aplicacion

---
+++ lib/pages/UsersVideos/search_json.dart
@@ -0,0 +1,176 @@
+const List searchJson = [
+  {
+    "img": "assets/mini_videos/1.png",
+    "title": "Que es lactancia materna y su importancia",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/2.1.png",
+    "title": "Beneficios de la lactancia materna",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/2.2.png",
+    "title": "Beneficios para la madre",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/3.1.png",
+    "title": "Que es el calostro, leche de transicion y leche madura",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/3.2.png",
+    "title": "Leche de transicion",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/3.3.png",
+    "title": "Leche Madura",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/3.4.png",
+    "title": "Composicion de la leche materna",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/4.1.png",
+    "title": "Como saber que el bebe se alimento lo suficiente",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/4.2.png",
+    "title": "Senales cuando el nino tiene hambre",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/5.png",
+    "title": "Hitos del peso a vigilar con el pediatra",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/6.png",
+    "title": "Higiene de manos, al ofrecer lactancia materna",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/7.png",
+    "title": "Lactancia materna en tiempos de Covid-19",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/8.1.png",
+    "title": "Que alimentacion debe recibir una madre durante la lactancia materna"
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/8.2.png",
+    "title": "Cafeina! Se encuentra en el cafe, chocolates y las sodas",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/9.png",
+    "title": "Puedo tomar medicamentos durante la lactancia materna?",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/10.png",
+    "title": "Posiciones y tecnicas de lactancia materna",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/11.1.png",
+    "title": "Como detectar signos y complicaciones en la lactancia materna",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/11.2.png",
+    "title": "Grietas en los pezones",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/11.3.png",
+    "title": "Mastitis, infeccion en las mamas",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/11.4.png",
+    "title": "Conductos obstruidos",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/11.5.png",
+    "title": "Congestion Mamaria",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/12.png",
+    "title": "Como realizar masajes al seno antes de realizar la lactancia materna",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/13.1.png",
+    "title": "Como inicar mi banco de leche en casa",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/13.2.png",
+    "title": "Extraccion manual de la leche",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/13.3.png",
+    "title": "Almacenamiento y conservacion",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/14.1.png",
+    "title": "Que leyes apoyan la lactancia materna en Panama Pt.1",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/14.2.png",
+    "title": "Que leyes apoyan la lactancia materna en Panama Pt.2",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/15.png",
+    "title": "Diferencias entre la leche materna y la leche de vaca",
+    //"video": ,
+  },
+
+  {
+    "img": "assets/mini_videos/16.png",
+    "title": "Mitos de la lactancia materna",
+    //"video": ,
+  },
+
+];
---
+++ lib/pages/UsersVideos/user_videos.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/pages/UsersVideos/search_json.dart';
+
+class User_videos extends StatefulWidget {
+  const User_videos({super.key});
+
+  @override
+  State<User_videos> createState() => _User_videosState();
+}
+
+class _User_videosState extends State<User_videos> {
+   int _selectedIndex = 0;
+   
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 12, 2, 80),
+      appBar: getAppBar(),
+      body: getBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.transparent, // Fondo transparente
+        selectedItemColor: Colors.white, // Color de ícono seleccionado
+        unselectedItemColor: Colors.grey.withOpacity(0.60), // Color de ícono no seleccionado
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favoritos',
+          ),
+        ],
+      ),
+    );
+  }
+
+  PreferredSizeWidget getAppBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Container(
+        height: 35,
+        width: double.infinity,
+        margin: EdgeInsets.only(top: 15),
+        decoration: BoxDecoration(
+          color: Colors.grey.withOpacity(0.40),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: "Buscar",
+            hintStyle: TextStyle(color: Colors.white.withOpacity(0.5)),
+            prefixIcon: Icon(
+              Icons.search,
+              color: Colors.white.withOpacity(0.5),
+            ),
+            contentPadding: EdgeInsets.symmetric(vertical: 10.0), 
+          ),
+        ),
+      ),
+    );
+  }
+
+  SingleChildScrollView getBody() {
+    var size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+     child: Padding(
+       padding: const EdgeInsets.only(top: 35, left: 20, right: 18),
+       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Videos Disponibles",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 12),
+          Column(
+            children: List.generate(searchJson.length, (index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Row(
+                  children: [
+                    Container(
+                      width: (size.width - 40) * 0.8,
+                      height: 80,
+                      child: Row(
+                        children: [
+                          Stack(
+                            children: [
+                              Container(
+                                width: 120,
+                                height: 70,
+                                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5),
+                                image: DecorationImage(
+                                    image: AssetImage(searchJson[index]['img']),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                width: 120,
+                                height: 70,
+                                decoration: BoxDecoration(
+                                  color: Colors.black.withOpacity(0.2),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(width: 15),
+                          Container(
+                            width: (size.width - 36) * 0.4,
+                            child: Text(
+                              searchJson[index]['title'],
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: (size.width - 36) * 0.2,
+                      height: 80,
+                      child: Center(
+                        child: Container(
+                          width: 35,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(width: 2, color: Colors.white),
+                          ),
+                          child: Center(
+                            child: Icon(
+                              Icons.play_arrow,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      )
+                    ),
+                  ],
+                ),
+              );
+            })
+          ),
+        ],
+       ),
+     ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+
+
