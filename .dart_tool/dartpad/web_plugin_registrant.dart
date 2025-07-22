1bb0f9c26ad1443c0214f6d973b6406b0d2ce00e
cesarMarquinez0220 <cesarmarquinez847@gmail.com>
2025-07-22 14:43:25 -0500

    nada

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -7,8 +7,15 @@
 // ignore_for_file: type=lint
 
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_performance_web/firebase_performance_web.dart';
+import 'package:flutter_inappwebview_web/web/main.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 import 'package:package_info_plus/src/package_info_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:video_player_web/video_player_web.dart';
 import 'package:wakelock_plus/src/wakelock_plus_web_plugin.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -16,8 +23,15 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   FirebaseFirestoreWeb.registerWith(registrar);
+  ConnectivityPlusWebPlugin.registerWith(registrar);
+  FirebaseAnalyticsWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FirebasePerformanceWeb.registerWith(registrar);
+  InAppWebViewFlutterPlugin.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
   PackageInfoPlusWebPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
   VideoPlayerPlugin.registerWith(registrar);
   WakelockPlusWebPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -3,21 +3,34 @@
 // This file is generated from template in file `flutter_tools/lib/src/flutter_plugins.dart`.
 //
 
-// @dart = 3.0
+// @dart = 3.8
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:flutter_inappwebview_android/flutter_inappwebview_android.dart';
 import 'package:path_provider_android/path_provider_android.dart';
+import 'package:shared_preferences_android/shared_preferences_android.dart';
+import 'package:sqflite_android/sqflite_android.dart';
 import 'package:video_player_android/video_player_android.dart';
+import 'package:flutter_inappwebview_ios/flutter_inappwebview_ios.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
+import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
+import 'package:sqflite_darwin/sqflite_darwin.dart';
 import 'package:video_player_avfoundation/video_player_avfoundation.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
+import 'package:shared_preferences_linux/shared_preferences_linux.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
+import 'package:flutter_inappwebview_macos/flutter_inappwebview_macos.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
+import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
+import 'package:sqflite_darwin/sqflite_darwin.dart';
 import 'package:video_player_avfoundation/video_player_avfoundation.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
+import 'package:flutter_inappwebview_windows/flutter_inappwebview_windows.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider_windows/path_provider_windows.dart';
+import 'package:shared_preferences_windows/shared_preferences_windows.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
 
 @pragma('vm:entry-point')
@@ -27,10 +40,37 @@ class _PluginRegistrant {
   static void register() {
     if (Platform.isAndroid) {
       try {
+        AndroidInAppWebViewPlatform.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_inappwebview_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         PathProviderAndroid.registerWith();
       } catch (err) {
         print(
           '`path_provider_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SharedPreferencesAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SqfliteAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`sqflite_android` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -46,10 +86,37 @@ class _PluginRegistrant {
 
     } else if (Platform.isIOS) {
       try {
+        IOSInAppWebViewPlatform.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_inappwebview_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         PathProviderFoundation.registerWith();
       } catch (err) {
         print(
           '`path_provider_foundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SharedPreferencesFoundation.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_foundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SqfliteDarwin.registerWith();
+      } catch (err) {
+        print(
+          '`sqflite_darwin` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -64,6 +131,15 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isLinux) {
+      try {
+        ConnectivityPlusLinuxPlugin.registerWith();
+      } catch (err) {
+        print(
+          '`connectivity_plus` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         PackageInfoPlusLinuxPlugin.registerWith();
       } catch (err) {
@@ -83,6 +159,15 @@ class _PluginRegistrant {
       }
 
       try {
+        SharedPreferencesLinux.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         WakelockPlusLinuxPlugin.registerWith();
       } catch (err) {
         print(
@@ -93,10 +178,37 @@ class _PluginRegistrant {
 
     } else if (Platform.isMacOS) {
       try {
+        MacOSInAppWebViewPlatform.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_inappwebview_macos` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         PathProviderFoundation.registerWith();
       } catch (err) {
         print(
           '`path_provider_foundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SharedPreferencesFoundation.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_foundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SqfliteDarwin.registerWith();
+      } catch (err) {
+        print(
+          '`sqflite_darwin` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -121,6 +233,15 @@ class _PluginRegistrant {
 
     } else if (Platform.isWindows) {
       try {
+        WindowsInAppWebViewPlatform.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_inappwebview_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         PackageInfoPlusWindowsPlugin.registerWith();
       } catch (err) {
         print(
@@ -134,6 +255,15 @@ class _PluginRegistrant {
       } catch (err) {
         print(
           '`path_provider_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SharedPreferencesWindows.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_windows` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
